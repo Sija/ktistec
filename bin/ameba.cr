15691be3160aa4195d0537c8a52af97c8a97fc06
Todd Sundsted <todd@sundsted.com>
2026-04-02 18:20:50 -0400

    Prevent alternatives in spec matchers.

--- bin/ameba.cr
+++ bin/ameba.cr
@@ -2,6 +2,7 @@ require "ameba"
 
 require "../src/ameba/ktistec/no_direct_factory_calls"
 require "../src/ameba/ktistec/no_imperative_factories"
+require "../src/ameba/ktistec/no_alternatives_in_specs"
 require "../src/ameba/ktistec/no_explicit_return_nil"
 require "../src/ameba/ktistec/no_focused_specs"
 require "../src/ameba/ktistec/no_pending_specs"
